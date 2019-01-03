311c0d8fa03000ae0942dba1f23b4eb756b10095
meidan.zemer <meidan@gmail.com>
2019-01-03 19:11:26 +0200

    change contact point

--- definitions.ts
+++ definitions.ts
@@ -1,8 +1,8 @@
 export type contactPoint = Readonly<{
-    id: string;
+    userId: string,
+    cpId : string;
     name: string;
     description: string;
-    imagePath: string;
     createDate: number;
     modifyData: number;
 }>;
